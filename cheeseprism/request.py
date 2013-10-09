9103a03c86db2336d816bf7c068579f9df25c37f
whitmo <d.w.morriss@gmail.com>
2013-10-08 20:15:30 -0500

    some gardening to make use of pyramid properly

--- cheeseprism/request.py
+++ cheeseprism/request.py
@@ -1,42 +1,49 @@
 from cheeseprism.index import IndexManager
 from path import path
-from pyramid.decorator import reify
-from pyramid.request import Request
 from pyramid.security import unauthenticated_userid
 
 
-class CPRequest(Request):
+def includeme(config):
+    for name, func in request_funcs().items():
+        reify = not getattr(func, 'skip_reify', False)
+        config.add_request_method(func, name=name, reify=reify)
+
+
+def request_funcs():
     """
-    CheesePrism request object
+    Returns a map of functions to apply to the request
+
+    No variables can be assigned for this work.
     """
-    imclass = IndexManager
+    def userid(request):
+        return unauthenticated_userid(request)
+    userid.skip_reify = True
 
-    @reify
-    def userid(self):
-        return unauthenticated_userid(self)
+    def settings(request):
+        return request.registry.settings
 
-    @reify
-    def settings(self):
-        return self.registry.settings
 
-    @reify
-    def index_templates(self):
-        return self.registry.settings['cheeseprism.index_templates']
+    def index_templates(request):
+        return request.registry.settings['cheeseprism.index_templates']
 
-    @reify
-    def file_root(self):
-        return path(self.registry.settings['cheeseprism.file_root'])
 
-    @reify
-    def index(self):
-        return self.imclass.from_settings(self.settings)
+    def file_root(request):
+        return path(request.registry.settings['cheeseprism.file_root'])
 
-    @reify
-    def index_data_path(self):
-        return self.index.datafile_path
 
-    @reify
-    def index_data(self):
-        return self.index.data_from_path(self.file_root / self.index_data_path)
-    
+    def index(request):
+        return IndexManager.from_settings(request.settings)
 
+
+    def index_data_path(request):
+        return request.index.datafile_path
+
+
+    def index_data(request):
+        return request.index.data_from_path(request.file_root / request.index_data_path)
+
+
+    def includeme(config):
+        config.add_request_method()
+
+    return locals()
--- cheeseprism/wsgiapp.py
+++ cheeseprism/wsgiapp.py
@@ -1,6 +1,5 @@
 from cheeseprism.auth import BasicAuthenticationPolicy
 from cheeseprism.index import EnvFactory
-from cheeseprism.request import CPRequest as Request
 from cheeseprism.resources import App
 from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
@@ -15,17 +14,17 @@ def main(global_config, **settings):
                           session_factory=session_factory,
                           authentication_policy=\
                           BasicAuthenticationPolicy(BasicAuthenticationPolicy.noop_check))
-    
+
     config.add_translation_dirs('locale/')
     config.include('pyramid_jinja2')
     config.add_renderer('.html', renderer_factory)
 
     config.add_static_view('static', 'static')
-    config.scan('cheeseprism.views')
-    config.scan('cheeseprism.index')
-    config.set_request_factory(Request)
-    config.add_route('package', 'package/{name}/{version}', view='cheeseprism.views.from_pypi')
+    config.include('.request')
+    config.scan('.views')
+    config.scan('.index')
+    config.add_route('package', 'package/{name}/{version}')
+    config.add_view('.views.from_pypi', route_name='package')
     settings['index_templates'] = EnvFactory.from_str(settings['cheeseprism.index_templates'])
 
     return config.make_wsgi_app()
-
