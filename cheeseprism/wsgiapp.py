fd345e1f155d5e77b1593cdbc7974c25e94dc1c0
whitmo <d.w.morriss@gmail.com>
2013-12-09 21:44:40 -0600

    namer and views
     - factor all view config to views.py `includeme`
     - set up resolution based on config for `namer`
     - use name in upload
     - add namer to request

--- cheeseprism/wsgiapp.py
+++ cheeseprism/wsgiapp.py
@@ -5,20 +5,19 @@ from functools import partial
 from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from pyramid.settings import asbool
-from pyramid_jinja2 import renderer_factory
 import futures
 import logging
 import multiprocessing
-import signal
-import sys
 import os
+import signal
+
 
 logger = logging.getLogger(__name__)
 
 
+
 def main(global_config, **settings):
     settings = dict(global_config, **settings)
-
     settings.setdefault('jinja2.i18n.domain', 'CheesePrism')
 
     session_factory = UnencryptedCookieSessionFactoryConfig('cheeseprism')
@@ -30,14 +29,10 @@ def main(global_config, **settings):
     setup_workers(config.registry)
 
     config.add_translation_dirs('locale/')
-    config.include('pyramid_jinja2')
-    config.add_renderer('.html', renderer_factory)
-    config.add_static_view('static', 'static')
+
     config.include('.request')
-    config.scan('.views')
+    config.include('.views')
     config.include('.index')
-    config.add_route('package', 'package/{name}/{version}')
-    config.add_view('.views.from_pypi', route_name='package')
 
     tempspec = settings.get('cheeseprism.index_templates', '')
     config.registry['cp.index_templates'] = EnvFactory.from_str(tempspec)
