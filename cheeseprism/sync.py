eb4e736258a60bd44f19fae111c225cf50bd6acc
whitmo <d.w.morriss@gmail.com>
2013-12-18 17:13:41 -0600

    work with new optimizations, respect not writing index html

--- cheeseprism/sync.py
+++ cheeseprism/sync.py
@@ -1,6 +1,6 @@
 from . import utils
 from .index import IndexManager
-from .index import notify_packages_added
+from .index import bulk_add_pkgs
 from path import path
 from threading import Thread
 import logging
@@ -35,12 +35,13 @@ def update_index(index, reg):
         new_pkgs = index.update_data()
         if index.path.exists(): #for testing
             with utils.benchmark("New packages notifier: %s" %new_pkgs):
-                pkg_added = list(notify_packages_added(index, new_pkgs, reg))
+                leaves, archs = bulk_add_pkgs(index, new_pkgs, reg)
 
-            if not all((index.datafile_path.exists(), index.home_file.exists())):
+            if index.write_index_html is True \
+              and not all((index.datafile_path.exists(), index.home_file.exists())):
                 index.write_index_home(index.projects_from_archives())
 
-            return pkg_added
+            return leaves, archs
 
 
 def wait_for_file(fp, sleep=0.2, max_tries=5):
