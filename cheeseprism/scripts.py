7372719e5a458c73d71ee01c8aa1ef30474843bf
whitmo <d.w.morriss@gmail.com>
2013-12-19 16:34:40 -0600

    safer master cleanse
     - symlink back old masters
     - copy original to off path directory
     - rename last

--- cheeseprism/scripts.py
+++ cheeseprism/scripts.py
@@ -4,18 +4,27 @@ import sys
 
 
 def mc(args=sys.argv[1:]):
+    def_save = path('old-master/files')
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Path to index folder", type=path)
-    parser.add_argument("--cp", help="copy rather than move", type=bool, action='store_true', default=False)
+    parser.add_argument("--copydir", help="dir to co", type=path, action='store', default=def_save)
+
     pargs = parser.parse_args(args)
     index = pargs.path.abspath()
+    copydir = pargs.copydir
+    if copydir == def_save:
+        copydir = index / copydir
+
+    copydir.makedirs_p()
 
     files = index.files('*-master.tar.gz')
 
     print "%s files" %len(files)
     for fp in index.files('*-master.tar.gz'):
         np = fp.parent / fp.replace('-master', '')
-        if not np.exists():
-            fp.copy(np)
-        else:
+        if np.exists():
             print "DUPE: %s" %np
+        else:
+            newfp = fp.rename(copydir / fp.basename())
+            newfp.symlink(index / newfp.basename())
+            newfp.copy(np)
