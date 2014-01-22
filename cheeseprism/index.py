710ffdd34f6ccd4ea72c4fd77e931b4e58ff78be
whitmo <d.w.morriss@gmail.com>
2014-01-22 10:19:04 -0600

    move archive pki_ff to own module

--- cheeseprism/index.py
+++ cheeseprism/index.py
@@ -2,6 +2,7 @@
 Classes, subscribers and functions for dealing with index management
 """
 from . import event
+from .archiveutil import ArchiveUtil
 from .desc import template
 from .desc import updict
 from .jenv import EnvFactory
@@ -16,76 +17,12 @@ from pyramid.path import DottedNameResolver as dnr
 from pyramid.settings import asbool
 import json
 import logging
-import pkginfo
 import re
 import threading
 import time
-import traceback
+
 
 logger = logging.getLogger(__name__)
-
-
-class ArchiveUtil(object):
-    """
-    A pickeable object we can pass via mp queues
-    """
-    EXTS = re.compile(r'^.*(?P<ext>\.egg|\.gz|\.bz2|\.tgz|\.zip|\.whl)$')
-
-    def read(self, (arch, data)):
-        md5 = arch.read_md5().encode('hex')
-        pkgdata = None
-        if not md5 in data:
-            pkgdata = self.arch_to_add_map(arch)
-        return md5, pkgdata
-    __call__ = read
-
-    def arch_to_add_map(self, arch, error_handler=None):
-        pkgi = self.pkginfo_from_file(arch, handle_error=error_handler)
-        if pkgi:
-            return self.pkginfo_to_pkgdata(arch, pkgi)
-
-    def pkginfo_to_pkgdata(self, arch, pkgi):
-        start = time.time()
-        return dict(name=pkgi.name,
-                    version=pkgi.version,
-                    filename=str(arch.name),
-                    added=start)
-
-    @staticmethod
-    def move_on_error(error_folder, exc, path_):
-        logger.error(traceback.format_exc())
-        path_.rename(path(error_folder) / path_.basename())
-
-    def extension_of(self, path):
-        match = self.EXTS.match(str(path))
-        if match:
-            return match.groupdict()['ext']
-
-    def pkginfo_from_file(self, path, handle_error=None):
-        ext = self.extension_of(path)
-        not_recognized = False
-        try:
-            if ext is not None:
-                if ext in set(('.gz','.tgz', '.bz2', '.zip')):
-                    return pkginfo.sdist.SDist(path)
-                elif ext == '.egg':
-                    return pkginfo.bdist.BDist(path)
-                elif ext == '.whl':
-                    return pkginfo.wheel.Wheel(path)
-            not_recognized = True
-        except Exception, e:
-            if handle_error is not None:
-                return handle_error(e, path)
-            raise
-
-        if not_recognized is True:
-            msg = "Unrecognized extension: %s" %path
-            e = RuntimeError("Unrecognized extension: %s" %path)
-            if handle_error is not None:
-                logger.error(msg)
-                return handle_error(e, path)
-
-            raise e
 
 
 class IndexManager(object):
@@ -195,7 +132,10 @@ class IndexManager(object):
 
     def regenerate_leaf(self, leafname):
         files = self.path.files('%s-*.*' %leafname)
-        versions = ((self.pkginfo_from_file(self.path / item, handle_error=self.move_on_error), item) for item in files)
+        
+        pki_ff = partial(self.at.pkginfo_from_file, handle_error=self.move_on_error)
+        versions = ((pki_ff(self.path / item), item) for item in files)
+
         return self.write_leaf(self.path / leafname, versions)
 
     def regenerate_all(self):
